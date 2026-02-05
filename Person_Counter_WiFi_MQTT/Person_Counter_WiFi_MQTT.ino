599de21ea5bb83ef96e2d4762d603c636658f74b
Erkki Räsänen <erkkiras@metropolia.fi>
2026-02-05 18:25:39 +0200

    added uri

--- Person_Counter_WiFi_MQTT/Person_Counter_WiFi_MQTT.ino
+++ Person_Counter_WiFi_MQTT/Person_Counter_WiFi_MQTT.ino
@@ -7,16 +7,18 @@
 
 #include <WiFi.h>
 #include <MQTT.h>
+#include "Ultrasonic.h"
+Ultrasonic ultrasonic(D7);
 
 ////// Give database information here:
 String myDB = "data_ml"; //unique database name
-String myColl = "p_count"; //unique collection name
-String myID = "aiot"; //serial number, mac address or other unique identification for your device
+String myColl = "p_count_2"; //unique collection name
+String myID = "robo"; //serial number, mac address or other unique identification for your device
 //////
 
 ////// Give WiFi credentials here
-const char ssid[] = "Erkki's Galaxy S23";
-const char pass[] = "xxxxxxx";
+const char ssid[] = "Robo_Device_net";
+const char pass[] = "RoboGarage2@25";
 //////
 
 WiFiClient net;
@@ -26,6 +28,7 @@ unsigned long lastMillis = 0; // MQTT send timer
 unsigned long lastMillis2 = 0; // pCount timer
 
 int pCount = 0;
+float sensor = 0;
 
 void connect() {
   Serial.print("checking wifi...");
@@ -35,7 +38,7 @@ void connect() {
   }
 
   Serial.print("\nconnecting...");
-  while (!client.connect("ESP-32-eki", "automaatio", "Z0od2PZF65jbtcXu")) {
+  while (!client.connect("ESP-32-eki2", "automaatio", "Z0od2PZF65jbtcXu")) {
     Serial.print(".");
     delay(1000);
   }
@@ -75,17 +78,25 @@ void loop() {
 ////// Persons are counted for 30 seconds and the total bumber of persons counted
 ////// is then sent to database
 
-int sensor = analogRead(A0);
-
-if (sensor > 250 && (millis() - lastMillis2 > 500)) {
-  Serial.println(sensor);
+/*
+if (millis() - lastMillis2 > 500) {
   lastMillis2 = millis();
+  sensor = ultrasonic.MeasureInCentimeters();
+  if(sensor < 100) pCount++;
+  //Serial.println(sensor);
+}
+*/
+
+sensor = ultrasonic.MeasureInCentimeters();
+if(sensor < 100) {
+  Serial.println(sensor);
   pCount++;
+  delay(1000);
 }
 
 
   // publish a message roughly every 30 seconds.
-  if (millis() - lastMillis > 30000) {
+  if (millis() - lastMillis > 300000) {
     lastMillis = millis();
 
 ///// Here, a JSON message to be published is parsed.
